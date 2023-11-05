aa64a70181a3659a375811595ddf4c0b77c370fb
misaku54 <eisei0504@outlook.jp>
2023-11-05 22:53:38 +0900

    [update]

--- donut/check.rb
+++ donut/check.rb
@@ -1,6 +1,8 @@
 module Check
   # 正しい数値かどうかをチェックする。
   def number_check(input)
-
+    return puts "型エラー、処理終了します。#{input}" unless input.instance_of?(integer)
+    return puts "範囲エラー、処理を終了します。#{input}" if input < 0
+    return puts "範囲エラー、処理を終了します。#{input}" if input > 100
   end
 end
