2ada4bd6727627464fbb51803d29946ac8d50f55
misaku54 <eisei0504@outlook.jp>
2023-11-05 22:31:30 +0900

    [update]

--- donut/picture.rb
+++ donut/picture.rb
@@ -1,5 +1,6 @@
 class Picture
-  attr
+  attr_accessor :height, :width, :donut_ary
+
   # 初期化
   def initialize
     @height = 0
@@ -16,11 +17,16 @@ class Picture
 
   # 絵を入力
   def input_picture
-
+    # 代入する時はselfが必要、参照する際はいらない。
+    print '縦方向のマス数：'
+    self.height = gets.chomp.to_i
+    print '横方向のます数：'
+    self.width  = gets.chomp.to_i
+    print '１行目：'
   end
 
   # 入力チェック
-  def check
+  def check(input)
 
   end
 
