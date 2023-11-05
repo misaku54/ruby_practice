589cb07c8a61dcd6d740509a5a7cdb7736f3e80d
misaku54 <eisei0504@outlook.jp>
2023-11-05 21:34:53 +0900

    [update]

---
+++ donut/picture.rb
@@ -0,0 +1,31 @@
+class Picture
+  attr
+  # 初期化
+  def initialize
+    @height = 0
+    @width  = 0
+    @donut_ary = []
+  end
+
+  # 実行部
+  def run
+
+  end
+
+  private
+
+  # 絵を入力
+  def input_picture
+
+  end
+
+  # 入力チェック
+  def check
+
+  end
+
+  # ドーナツ数をカウント
+  def donut_count
+
+  end
+end
