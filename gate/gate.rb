af9b761987068df65f96e91512c20f894181640f
misaku54 <eisei0504@outlook.jp>
2023-11-08 16:02:50 +0900

    [update]

--- gate/gate.rb
+++ gate/gate.rb
@@ -1,10 +1,11 @@
 class Gate
-  attr_accessor :route_c, :station_c
+  attr_accessor :route_c, :station_c, :fee_ary
 
   # 初期化
   def initialize
     @route_c = 0
     @station_c = 0
+    @fee_ary = []
   end
 
   # 実行
@@ -24,7 +25,11 @@ class Gate
 
   # インプット運賃料金
   def input_fee
-
+    counter = 1
+    while counter <= route_c do
+      tmp = gets.chomp.split.map(&:to_i)
+      self.fee_ary << tmp
+    end
   end
 
   # 運賃計算
