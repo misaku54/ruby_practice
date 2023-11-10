69820616fd620f277642f24dc43407b57e18a9c0
misaku54 <eisei0504@outlook.jp>
2023-11-10 19:03:08 +0900

    [update]

---
+++ wheel/wheel.rb
@@ -0,0 +1,27 @@
+class Wheel
+  attr_accessor :gondola_num, :group_num, :ride_limit_ary, :group_count_ary
+
+  # 初期化
+  def initialize
+    @gondola_num = 0
+    @group_num = 0
+    @ride_limit_ary  = []
+    @group_count_ary = []
+  end
+
+  # インプット
+  def input_gond_grop_number
+    tmp = gets.chomp.split.map(&:to_i)
+    self.gondola_num = tmp[0]
+    self.group_num = tmp[1]
+  end
+
+  def input_ride_limit
+    i = 1
+    while i <= gondola_num do
+      print "#{i}番のゴンドラの乗車可能人数："
+      self.ride_limit_ary << gets.chomp.to_i
+      i += 1
+    end
+  end
+end
