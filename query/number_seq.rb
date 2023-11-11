09d2559999399b149bb558a67d505fb251e30b8d
misaku54 <eisei0504@outlook.jp>
2023-11-12 05:17:57 +0900

    [update]

---
+++ query/number_seq.rb
@@ -0,0 +1,28 @@
+class NumberSeq
+  attr_accessor :numbers
+
+  # 初期化
+  def initialize
+    @numbers = []
+  end
+
+  # 実行
+  def run
+    input_ary
+  end
+
+  private 
+
+  def input_ary
+    ele_count  = gets.chomp.to_i
+    
+    i = 0
+    while i < ele_count do
+      tmp = gets.chomp.to_i
+      numbers.push(tmp)
+    end
+
+    p numbers
+  end
+
+end
