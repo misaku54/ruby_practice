827538b51ef7f25e07cd34bdc362da8b8d40f4b5
misaku54 <eisei0504@outlook.jp>
2023-11-17 14:51:14 +0900

    [update]

--- day/day.rb
+++ day/day.rb
@@ -1,1 +1,19 @@
+# 二次元配列で管理する。
 ary = [[1,4],[2,3],[5,7],[8,15]]
+
+ary.each.with_index do |ar, index|
+  front = ar[0]
+  back  = ar[1] 
+
+  if index = 0
+    tmp_ary.push(front)
+    tmp_ary.push(back)
+    next
+  end
+  
+  # 最大と最小を求める。a = b, b = a + 1, b < a
+  if front = 
+    
+
+  
+end
