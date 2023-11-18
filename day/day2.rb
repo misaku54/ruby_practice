468645cfda9f06c00c1f5f9f7b6935ef16fdb803
misaku54 <eisei0504@outlook.jp>
2023-11-18 15:44:53 +0900

    [update]

--- day/day2.rb
+++ day/day2.rb
@@ -4,14 +4,15 @@ tmp_ary = []
 n = gets.chomp.to_i
 
 n.times do
-  tmp =  gets.chomp.split.map(&:to_i)
-  raise if tmp[0] > tmp[1]
-  (tmp[0]..tmp[1]).each {|a| tmp_ary << a}
+  x,y =  gets.chomp.split.map(&:to_i)
+  # raise if tmp[0] > tmp[1]
+  ary = [*x..y]
+  ary.each {|a| tmp_ary << a.to_i}
 end
 
-p tmp_ary
+# p tmp_ary
 kinmu_ary = tmp_ary.uniq.sort
-p kinmu_ary
+# p kinmu_ary
 
 
 mae_ar = 0
@@ -20,21 +21,28 @@ max_kinmu_ary = []
 
 
 kinmu_ary.each.with_index(0) do |ar, index|
+  if index == kinmu_ary.size - 1
+    kinmu_count += 1
+    max_kinmu_ary << kinmu_count
+    break
+  end
+
   if index == 0
     kinmu_count += 1
+    mae_ar = ar.to_i
     next
   end
 
   # 順番に増えていれば勤務カウントを＋１
   if ar == mae_ar + 1
     kinmu_count += 1
-    mae_ar = ar
+    mae_ar = ar.to_i
     next
   # そうでなければ、勤務カウントを配列に格納し、リセットする。
   end
   max_kinmu_ary << kinmu_count
   kinmu_count = 1
-  mae_ar = ar
+  mae_ar = ar.to_i
 end
 
-p max_kinmu_ary
+puts max_kinmu_ary.max
