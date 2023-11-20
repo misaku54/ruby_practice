92d95477a5ac568ca040bde47f6219e1c31bf606
misaku54 <eisei0504@outlook.jp>
2023-11-21 04:24:42 +0900

    [update]

--- day/slide.rb
+++ day/slide.rb
@@ -1,7 +1,12 @@
+# 右スライド
 def minus_slide
   return slide_count if 
-  if a[y][x] == a[y][x + 1] - 1
+  if row[y][x] == row[y][x + 1] - 1
     slide_count += 1
 
   end
+end
+
+ary.each do |row|
+
 end
