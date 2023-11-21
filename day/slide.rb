7e2169588149f33cb3d352bffb0630673a621efa
misaku54 <eisei0504@outlook.jp>
2023-11-21 21:18:04 +0900

    [update]

--- day/slide.rb
+++ day/slide.rb
@@ -1,18 +1,78 @@
+slide_ary = []
+slide_count = 1
+
+n = gets.chomp.to_i
+ary = []
+
 # 右スライド
-def minus_slide(row,ar)
+def right_slide_minus(row,ar,n,slide_count,slide_ary)
   next_x = row.index(ar) + 1
   if next_x >= n
-    slide_ary << slide_count 
-    slide_count = 0
+    slide_ary << slide_count
+    p slide_ary 
+    slide_count = 1
+    return
   end
   if ar == row[next_x] - 1
     slide_count += 1
-    minus_slide(row, next_x)
+    right_slide_minus(row, row[next_x],n,slide_count,slide_ary)
   end
 end
 
-ary.each do |row|
-  row.each do |ar|
-    minus_slide(row,ar)
+# 右スライド
+def right_slide_plus(row,ar,n)
+  next_x = row.index(ar) + 1
+  if next_x >= n
+    slide_ary << slide_count 
+    slide_count = 1
+    return
+  end
+  if ar == row[next_x] + 1
+    slide_count += 1
+    right_slide_plus(row, row[next_x],n)
   end
 end
+
+# 左スライド
+def left_slide_plus(row,ar,n)
+  mae_x = row.index(ar) - 1
+  if mae_x < 0
+    slide_ary << slide_count 
+    slide_count = 1
+    return
+  end
+  if ar == row[mae_x] + 1
+    slide_count += 1
+    left_slide_plus(row, row[mae_x],n)
+  end
+end
+
+# 左スライド
+def left_slide_minus(row,ar,n)
+  mae_x = row.index(ar) - 1
+  if mae_x < 0
+    slide_ary << slide_count 
+    slide_count = 1
+    return
+  end
+  if ar == row[mae_x] - 1
+    slide_count += 1
+    left_slide_minus(row, row[mae_x],n)
+  end
+end
+
+
+n.times do
+  ary << gets.chomp.split('').map(&:to_i)
+end
+p ary
+ary.each do |row|
+  row.each do |ar|
+    right_slide_minus(row,ar,n,slide_count,slide_ary)
+    # right_slide_plus(row,ar,n)
+    # left_slide_minus(row,ar,n)
+    # left_slide_plus(row,ar,n)
+  end
+end
+
+
