f3ae07773750bd5b209c284a4a161422803e638f
misaku54 <eisei0504@outlook.jp>
2023-11-21 21:54:41 +0900

    [update]

--- A/rank_c.rb
+++ A/rank_c.rb
@@ -1,6 +1,10 @@
 h,w,n = gets.chomp.split.map(&:to_i)
 banmen_ary = []
 
+# 盤面格納
 h.times do
-  banmen_ary << gets.chomp.split('')
+  tmp = gets.chomp.split('')
+  raise if tmp.size > n 
+  banmen_ary << tmp
 end
+
